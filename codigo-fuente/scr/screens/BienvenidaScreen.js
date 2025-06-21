960865868dc0b24eda7f744e772d0e22be29df64
majosetoro <toromajose@gmail.com>
2025-06-21 13:41:12 -0300

    feat [HU-03]: formulario de registro de mascota

--- codigo-fuente/scr/screens/BienvenidaScreen.js
+++ codigo-fuente/scr/screens/BienvenidaScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-export default function BienvenidaScreen() {
+export default function BienvenidaScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 24 }}>¡Bienvenido a Pawtitas!</Text>
+      <Button
+        title="Registrar Mascota"
+        onPress={() => navigation.navigate('PetScreen')}
+      />
     </View>
   );
 }
